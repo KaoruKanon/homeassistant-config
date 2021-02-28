b754a0dde8438a5cf5845948af8a4ae5fabb4f1b
William Colpaert <40307380+KaoruKanon@users.noreply.github.com>
2021-02-28 21:06:54 +0100

    update

--- www/community/kiosk-mode/kiosk-mode.js
+++ www/community/kiosk-mode/kiosk-mode.js
@@ -1,50 +1,201 @@
-"use strict";function e(e,t){return o(e)||r(e,t)||u(e,t)||n()}function n(){throw new TypeError(
-"Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}
-function r(e,t){if("undefined"!=typeof Symbol&&Symbol.iterator in Object(e)){var n=[],r=!0,o=!1,i=void 0;try{for(var a,s=e[Symbol.iterator]();!(r=(
-a=s.next()).done)&&(n.push(a.value),!t||n.length!==t);r=!0);}catch(e){o=!0,i=e}finally{try{r||null==s.return||s.return()}finally{if(o)throw i}}
-return n}}function o(e){if(Array.isArray(e))return e}function t(e){return s(e)||a(e)||u(e)||i()}function i(){throw new TypeError(
-"Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}function a(e){
-if("undefined"!=typeof Symbol&&Symbol.iterator in Object(e))return Array.from(e)}function s(e){if(Array.isArray(e))return l(e)}function c(e,t,n,r,o,i,
-a){try{var s=e[i](a),c=s.value}catch(e){return void n(e)}s.done?t(c):Promise.resolve(c).then(r,o)}function d(s){return function(){var e=this,
-a=arguments;return new Promise(function(t,n){var r=s.apply(e,a);function o(e){c(r,t,n,o,i,"next",e)}function i(e){c(r,t,n,o,i,"throw",e)}o(void 0)})}}
-function _(e,t){var n;if("undefined"==typeof Symbol||null==e[Symbol.iterator]){if(Array.isArray(e)||(n=u(e))||t&&e&&"number"==typeof e.length){n&&(e=n
-);var r=0,t=function(){};return{s:t,n:function(){return r>=e.length?{done:!0}:{done:!1,value:e[r++]}},e:function(e){throw e},f:t}}throw new TypeError(
-"Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}var o,i=!0,
-a=!1;return{s:function(){n=e[Symbol.iterator]()},n:function(){var e=n.next();return i=e.done,e},e:function(e){a=!0,o=e},f:function(){try{
-i||null==n.return||n.return()}finally{if(a)throw o}}}}function u(e,t){if(e){if("string"==typeof e)return l(e,t);var n=Object.prototype.toString.call(e
-).slice(8,-1);return"Object"===n&&e.constructor&&(n=e.constructor.name),"Map"===n||"Set"===n?Array.from(e
-):"Arguments"===n||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)?l(e,t):void 0}}function l(e,t){(null==t||t>e.length)&&(t=e.length);for(var n=0,
-r=new Array(t);n<t;n++)r[n]=e[n];return r}var g,h,f,S,x,E=0;function m(){var e,
-t=0<arguments.length&&void 0!==arguments[0]?arguments[0]:h.querySelector("ha-panel-lovelace");!M("disable_km")&&t&&(e=g.hass.panelUrl,
-window.kioskModeEntities[e]||(window.kioskModeEntities[e]=[]),y(t,e))}function y(t,n){E++;try{var e=t.lovelace.config.kiosk_mode||{};b(t,e,n)}catch(e
-){E<40?setTimeout(function(){return y(t)},50):(console.log("Lovelace config not found, continuing with default configuration."),b(t,{},n))}}
-function A(e){return Array.isArray(e)?e:[e]}function M(e){return A(e).some(function(e){return window.location.search.includes(e)})}function j(e,t){A(e
-).forEach(function(e){return window.localStorage.setItem(e,t)})}function q(e){return"true"==window.localStorage.getItem(e)}function p(e){
-return e.querySelector("#kiosk_mode_".concat(e.localName))}function I(e,t){var n;p(t)||((n=document.createElement("style")).setAttribute("id",
-"kiosk_mode_".concat(t.localName)),n.innerHTML=e,t.appendChild(n))}function O(e){A(e).forEach(function(e){p(e)&&e.querySelector("#kiosk_mode_".concat(
-e.localName)).remove()})}function b(e,t,n){E=0;var r=e.shadowRoot.querySelector("hui-root").shadowRoot,o=r.querySelector("app-toolbar"),
-i=g.hass.states,a=t.admin_settings,s=t.non_admin_settings,c=t.entity_settings,d=t.user_settings,u=t.mobile_settings,l=!1,h=!1,f=q("kmHeader")||M([
-"kiosk","hide_header"]),e=(m=q("kmSidebar")||M(["kiosk","hide_sidebar"]))||f,f=e?f:t.kiosk||t.hide_header,m=e?m:t.kiosk||t.hide_sidebar;if(
-a&&x.is_admin&&(f=a.kiosk||a.hide_header,m=a.kiosk||a.hide_sidebar,l=a.ignore_entity_settings,h=a.ignore_mobile_settings),s&&!x.is_admin&&(
-f=s.kiosk||s.hide_header,m=s.kiosk||s.hide_sidebar,l=s.ignore_entity_settings,h=s.ignore_mobile_settings),d){var y=_(A(d));try{for(y.s();!(p=y.n()
-).done;){var p=p.value;A(p.users).some(function(e){return e.toLowerCase()==x.name.toLowerCase()})&&(f=p.kiosk||p.hide_header,m=p.kiosk||p.hide_sidebar
-,l=p.ignore_entity_settings,h=p.ignore_mobile_settings)}}catch(e){y.e(e)}finally{y.f()}}if(u&&!h&&(d=u.custom_width||812,window.innerWidth<=d&&(
-f=u.kiosk||u.hide_header,m=u.kiosk||u.hide_sidebar,l=u.ignore_entity_settings)),c&&!l){var b=_(c);try{for(b.s();!(w=b.n()).done;){var k=w.value,
-v=Object.keys(k.entity)[0],w=k.entity[v];window.kioskModeEntities[n].includes(v)||window.kioskModeEntities[n].push(v),i[v].state==w&&(
-"hide_header"in k&&(f=k.hide_header),"hide_sidebar"in k&&(m=k.hide_sidebar),"kiosk"in k&&(f=m=k.kiosk))}}catch(e){b.e(e)}finally{b.f()}}f?(I(
-"#view{min-height:100vh !important;--header-height:0;}app-header{display:none;}",r),M("cache")&&j("kmHeader","true")):O(r),m?(I(
-":host{--app-drawer-width:0 !important;}#drawer{display:none;}",S),I("ha-menu-button{display:none !important;}",o),M("cache")&&j("kmSidebar","true")
-):O([o,S]),window.dispatchEvent(new Event("resize"))}function k(){return v.apply(this,arguments)}function v(){return(v=d(regeneratorRuntime.mark(
-function e(){return regeneratorRuntime.wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,window.hassConnection;case 2:
-e.sent.conn.subscribeMessage(function(e){var t=window.kioskModeEntities[g.hass.panelUrl]||[];!t.length||"state_changed"!=e.event_type||!t.includes(
-e.data.entity_id)||e.data.old_state&&e.data.new_state.state==e.data.old_state.state||m()},{type:"subscribe_events",event_type:"state_changed"})
-;case 3:case"end":return e.stop()}},e)}))).apply(this,arguments)}function w(){new MutationObserver(function(e){e.forEach(function(e){
-e.addedNodes.forEach(function(e){"ha-panel-lovelace"==e.localName&&m(e)})})}).observe(f,{childList:!0})}window.kioskModeEntities={},Promise.resolve(
-customElements.whenDefined("hui-view")).then(function(){g=document.querySelector("home-assistant"),h=g.shadowRoot.querySelector("home-assistant-main"
-).shadowRoot,f=h.querySelector("partial-panel-resolver"),S=h.querySelector("app-drawer-layout"),x=g.hass.user,m(),w(),k()}),M("clear_km_cache")&&j([
-"kmHeader","kmSidebar"],"false");for(var R={header:"%c≡ kiosk-mode".padEnd(27),ver:"%cversion 1.6.5 "},C="%c\n",L=Math.max.apply(Math,t(Object.values(
-R).map(function(e){return e.length}))),N=0,T=Object.entries(R);N<T.length;N++){var H=e(T[N],1),P=H[0];R[P].length<=L&&(R[P]=R[P].padEnd(L)),
-"header"==P&&(R[P]="".concat(R[P].slice(0,-1),"⋮ "))}
-var U="display:inline-block;border-width:1px 1px 0 1px;border-style:solid;border-color:#424242;color:white;background:#03a9f4;font-size:12px;padding:4px 4.5px 5px 6px;"
-,z="border-width:0px 1px 1px 1px;padding:7px;background:white;color:#424242;line-height:0.7;";console.info(R.header+C+R.ver,U,"","".concat(U," "
-).concat(z));
+class KioskMode {
+  constructor() {
+    window.kioskModeEntities = {};
+    if (this.queryString("clear_km_cache")) this.setCache(["kmHeader", "kmSidebar"], "false");
+    this.ha = document.querySelector("home-assistant");
+    this.main = this.ha.shadowRoot.querySelector("home-assistant-main").shadowRoot;
+    this.user = this.ha.hass.user;
+    this.llAttempts = 0;
+    this.run();
+    this.entityWatch();
+    new MutationObserver(this.watchDashboards).observe(this.main.querySelector("partial-panel-resolver"), {
+      childList: true,
+    });
+  }
+
+  run(lovelace = this.main.querySelector("ha-panel-lovelace")) {
+    if (this.queryString("disable_km") || !lovelace) return;
+    this.getConfig(lovelace);
+  }
+
+  getConfig(lovelace) {
+    this.llAttempts++;
+    try {
+      const llConfig = lovelace.lovelace.config;
+      const config = llConfig.kiosk_mode || {};
+      this.processConfig(lovelace, config);
+    } catch {
+      if (this.llAttempts < 100) {
+        setTimeout(() => this.getConfig(lovelace), 50);
+      } else {
+        console.log("Lovelace config not found, continuing with default configuration.");
+        this.processConfig(lovelace, {});
+      }
+    }
+  }
+
+  processConfig(lovelace, config) {
+    const dash = this.ha.hass.panelUrl;
+    if (!window.kioskModeEntities[dash]) window.kioskModeEntities[dash] = [];
+    this.hideHeader = this.hideSidebar = this.ignoreEntity = this.ignoreMobile = false;
+
+    // Retrieve localStorage values & query string options.
+    const queryStringsSet =
+      this.cached(["kmHeader", "kmSidebar"]) || this.queryString(["kiosk", "hide_sidebar", "hide_header"]);
+    if (queryStringsSet) {
+      this.hideHeader = this.cached("kmHeader") || this.queryString(["kiosk", "hide_header"]);
+      this.hideSidebar = this.cached("kmSidebar") || this.queryString(["kiosk", "hide_sidebar"]);
+    }
+
+    // Use config values only if config strings and cache aren't used.
+    this.hideHeader = queryStringsSet ? this.hideHeader : config.kiosk || config.hide_header;
+    this.hideSidebar = queryStringsSet ? this.hideSidebar : config.kiosk || config.hide_sidebar;
+
+    const adminConfig = this.user.is_admin ? config.admin_settings : config.non_admin_settings;
+    if (adminConfig) for (let conf of adminConfig) this.setOptions(conf);
+
+    if (config.user_settings) {
+      for (let conf of this.array(config.user_settings)) {
+        if (this.array(conf.users).some((x) => x.toLowerCase() == this.user.name.toLowerCase())) this.setOptions(conf);
+      }
+    }
+
+    const mobileConfig = this.ignoreMobile ? null : config.mobile_settings;
+    if (mobileConfig) {
+      const mobileWidth = mobileConfig.custom_width ? mobileConfig.custom_width : 812;
+      if (window.innerWidth <= mobileWidth) this.setOptions(mobileConfig);
+    }
+
+    const entityConfig = this.ignoreEntity ? null : config.entity_settings;
+    if (entityConfig) {
+      for (let conf of entityConfig) {
+        const entity = Object.keys(conf.entity)[0];
+        if (!window.kioskModeEntities[dash].includes(entity)) window.kioskModeEntities[dash].push(entity);
+        if (this.ha.hass.states[entity].state == conf.entity[entity]) {
+          if ("hide_header" in conf) this.hideHeader = conf.hide_header;
+          if ("hide_sidebar" in conf) this.hideSidebar = conf.hide_sidebar;
+          if ("kiosk" in conf) this.hideHeader = this.hideSidebar = conf.kiosk;
+        }
+      }
+    }
+
+    this.insertStyles(lovelace);
+  }
+
+  insertStyles(lovelace) {
+    const huiRoot = lovelace.shadowRoot.querySelector("hui-root").shadowRoot;
+    const drawerLayout = this.main.querySelector("app-drawer-layout");
+    const appToolbar = huiRoot.querySelector("app-toolbar");
+
+    if (this.hideHeader) {
+      this.addStyle("#view{min-height:100vh !important;--header-height:0;}app-header{display:none;}", huiRoot);
+      if (this.queryString("cache")) this.setCache("kmHeader", "true");
+    } else {
+      this.removeStyle(huiRoot);
+    }
+
+    if (this.hideSidebar) {
+      this.addStyle(":host{--app-drawer-width:0 !important;}#drawer{display:none;}", drawerLayout);
+      this.addStyle("ha-menu-button{display:none !important;}", appToolbar);
+      if (this.queryString("cache")) this.setCache("kmSidebar", "true");
+    } else {
+      this.removeStyle([appToolbar, drawerLayout]);
+    }
+
+    // Resize window to "refresh" view.
+    window.dispatchEvent(new Event("resize"));
+
+    this.llAttempts = 0;
+  }
+
+  // Run on dashboard change.
+  watchDashboards(mutations) {
+    mutations.forEach(({ addedNodes }) => {
+      for (let node of addedNodes) if (node.localName == "ha-panel-lovelace") window.KioskMode.run(node);
+    });
+  }
+
+  // Run on entity change.
+  async entityWatch() {
+    (await window.hassConnection).conn.subscribeMessage((e) => this.entityWatchCallback(e), {
+      type: "subscribe_events",
+      event_type: "state_changed",
+    });
+  }
+
+  entityWatchCallback(event) {
+    const entities = window.kioskModeEntities[this.ha.hass.panelUrl] || [];
+    if (
+      entities.length &&
+      event.event_type == "state_changed" &&
+      entities.includes(event.data.entity_id) &&
+      (!event.data.old_state || event.data.new_state.state != event.data.old_state.state)
+    ) {
+      this.run();
+    }
+  }
+
+  setOptions(config) {
+    this.hideHeader = config.kiosk || config.hide_header;
+    this.hideSidebar = config.kiosk || config.hide_sidebar;
+    this.ignoreEntity = config.ignore_entity_settings;
+    this.ignoreMobile = config.ignore_mobile_settings;
+  }
+
+  // Convert to array.
+  array(x) {
+    return Array.isArray(x) ? x : [x];
+  }
+
+  // Return true if keyword is found in query strings.
+  queryString(keywords) {
+    return this.array(keywords).some((x) => window.location.search.includes(x));
+  }
+
+  // Set localStorage item.
+  setCache(k, v) {
+    this.array(k).forEach((x) => window.localStorage.setItem(x, v));
+  }
+
+  // Retrieve localStorage item as bool.
+  cached(key) {
+    return this.array(key).some((x) => window.localStorage.getItem(x) == "true");
+  }
+
+  styleExists(elem) {
+    return elem.querySelector(`#kiosk_mode_${elem.localName}`);
+  }
+
+  addStyle(css, elem) {
+    if (!this.styleExists(elem)) {
+      const style = document.createElement("style");
+      style.setAttribute("id", `kiosk_mode_${elem.localName}`);
+      style.innerHTML = css;
+      elem.appendChild(style);
+    }
+  }
+
+  removeStyle(elements) {
+    this.array(elements).forEach((elem) => {
+      if (this.styleExists(elem)) elem.querySelector(`#kiosk_mode_${elem.localName}`).remove();
+    });
+  }
+}
+
+// Overly complicated console tag.
+const conInfo = { header: "%c≡ kiosk-mode".padEnd(27), ver: "%cversion *DEV " };
+const br = "%c\n";
+const maxLen = Math.max(...Object.values(conInfo).map((el) => el.length));
+for (const [key] of Object.entries(conInfo)) {
+  if (conInfo[key].length <= maxLen) conInfo[key] = conInfo[key].padEnd(maxLen);
+  if (key == "header") conInfo[key] = `${conInfo[key].slice(0, -1)}⋮ `;
+}
+const header =
+  "display:inline-block;border-width:1px 1px 0 1px;border-style:solid;border-color:#424242;color:white;background:#03a9f4;font-size:12px;padding:4px 4.5px 5px 6px;";
+const info = "border-width:0px 1px 1px 1px;padding:7px;background:white;color:#424242;line-height:0.7;";
+console.info(conInfo.header + br + conInfo.ver, header, "", `${header} ${info}`);
+
+// Initial Run
+Promise.resolve(customElements.whenDefined("hui-view")).then(() => {
+  window.KioskMode = new KioskMode();
+});
